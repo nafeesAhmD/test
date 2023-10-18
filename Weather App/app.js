de66a21ce3ec7b4af6b007256892777b0baa473c
nafeesAhmD <nafeesahmed2899@gmail.com>
2023-10-18 21:13:18 +0500

    added file

--- Weather App/app.js
+++ Weather App/app.js
@@ -3,6 +3,7 @@ const searchBtn = document.querySelector('.search-btn')
 const weatherImg = document.querySelector('.weather-img')
 const temp = document.querySelector('.temp')
 const description = document.querySelector('.description')
+const location1 = document.querySelector('.location')
 const humidity = document.querySelector('#humidity')
 const windSpeed = document.querySelector('#wind-speed')
 
@@ -18,11 +19,33 @@ async function checkWeather(city){
     
     temp.innerHTML = `${Math.round(weatherData.main.temp - 273.15)}°C`
     description.innerHTML = `${weatherData.weather[0].description}`
+    location1.innerHTML = `${weatherData.name}`
+
     humidity.innerHTML = `${weatherData.main.humidity}%`
     windSpeed.innerHTML = `${weatherData.wind.speed}Km/H`
     
+    if(weatherData.weather[0].main == "Clouds"){
+        weatherImg.src = "./Images/clouds.png"
+    }
+    else if(weatherData.weather[0].main == "Clear"){
+        weatherImg.src = "./Images/clear.png"
+    }
+    else if(weatherData.weather[0].main == "Drizzle"){
+        weatherImg.src = "./Images/drizzle.png"
+    }
+    else if(weatherData.weather[0].main == "Rain"){
+        weatherImg.src = "./Images/rain.png"
+    }
+    else if(weatherData.weather[0].main == "Mist"){
+        weatherImg.src = "./Images/mist.png"
+    }
 }
 
 searchBtn.addEventListener('click',function(){
     checkWeather(inputBox.value)
+    inputValEmpty()
+         
 })
+function inputValEmpty(){
+    inputBox.value = ""
+}
