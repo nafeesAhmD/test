f446a57ffc7c7492e09b82bf8e7c954fa6102fe3
nafeesAhmD <nafeesahmed2899@gmail.com>
2024-06-27 14:23:43 +0500

    ok work doone

---
+++ Chapter 1 to 7/app.js
@@ -0,0 +1,62 @@
+
+// chapter2
+// ques1
+// var name = prompt("Enter name");
+// alert(name);
+// ques2
+// var name = "SSUET";
+// alert(name);
+// ques3
+// var pizza = "Pizza\n"+ "Pizz\n"+"Piz\n"+"Pi\n"+"P"
+// alert(pizza);
+// ques4
+// var email = prompt("Enter Email");
+// alert("My email address is" +" "+email);
+
+
+// chapter3
+// ques1
+// var age = prompt("Enter age");
+// alert("I am"+" " +age+ " "+ "years old");
+// ques2
+// var visit = +prompt("website visit");
+// alert("You have visit this site" +" "+visit+" "+ "times")
+
+
+// chapter4
+// ques1
+// var variable1 = "Hello"
+// var variable2 = "World"
+// var variable3 = "Testing"
+// ques2
+// legal character 
+//  name, $name,_name,name12,firstName;     
+// Ilegal character 
+// 12name,-name, @name, a bc, function
+// ques3
+// alert() 
+
+
+// chapter5,6,7
+// ques1
+// var num1 = +prompt('Enter value');
+// var num2 = +prompt('Enter value');
+// var add = num1 + num2 
+// alert("sum of" +" "+ num1 +" "+ "and"+" "+ num2  +" "+ "is" + " "+ add  );
+
+
+// var num1 = +prompt('Enter value');
+// var num2 = +prompt('Enter value');
+// var sub = num1 - num2 
+// alert("Result of" +" " + " "+ "Subtraction"+" "+ num1+ " "+"from"+ " "+ num2  +" "+ "is" + " "+ sub); 
+
+
+// var num1 = +prompt('Enter value');
+// var num2 = +prompt('Enter value');
+// var mult = num1 * num2 
+// alert("Result of" +" " + " "+ "Multiplication"+" "+ num1+ " "+"and"+ " "+ num2  +" "+ "is" + " "+ mult); 
+
+// var num1 = +prompt('Enter value');
+// var num2 = +prompt('Enter value');
+// var div = num1 / num2 
+// alert("Result of" +" " + " "+ "Division"+" "+ num1+ " "+"and"+ " "+ num2  +" "+ "by" + " "+ div); 
